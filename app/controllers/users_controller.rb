e2bf35a9fa8b1a315ba23d33cd9b8f197c92b269
MohamedAsan <mohamed.asan@freshworks.com>
2018-01-03 01:29:44 +0530

    Increase users shown in a page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -39,7 +39,7 @@ class UsersController < ApplicationController
   end
 
   def index
-    @users = User.paginate(page: params[:page], :per_page => 10)
+    @users = User.paginate(page: params[:page], :per_page => 15)
   end
 
   def destroy
