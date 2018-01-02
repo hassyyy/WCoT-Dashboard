b033eaef5dddc4d0651c80c610ff148693ce1939
MohamedAsan <mohamed.asan@freshworks.com>
2018-01-03 01:01:34 +0530

    Do not sign in again after user updated by different user
    Bug:
    When an account admin edits an user, the account admin is logged out and the edited user is logged in.
    
    To preven this, the user is logged in only when the user makes changes to his/her own profile.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -31,7 +31,7 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     if @user.update_attributes(params[:user])
       flash[:success] = "Profile updated"
-      sign_in @user
+      sign_in @user if current_user == @user
       redirect_to @user
     else
       render 'edit'
