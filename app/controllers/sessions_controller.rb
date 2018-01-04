c381a7c3c5bd8e82270daeb48171435dfa8acf8e
MohamedAsan <mohamed.asan@freshworks.com>
2018-01-04 22:36:22 +0530

    Show appropriate error message on login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,11 +5,16 @@ class SessionsController < ApplicationController
 
   def create
     user = User.find_by_email(params[:session][:user_identifier].downcase) || User.find_by_phone(params[:session][:user_identifier])
-    if user && user.authenticate(params[:session][:password])
-      sign_in user
-      redirect_back_or user
+    if user
+      if user.authenticate(params[:session][:password])
+        sign_in user
+        redirect_back_or user
+      else
+        flash.now[:error] = 'Invalid password'
+        render 'new'
+      end
     else
-      flash.now[:error] = 'Invalid email/password combination' # Not quite right!
+      flash.now[:error] = 'User not found. Check your email or phone number'
       render 'new'
     end
   end
