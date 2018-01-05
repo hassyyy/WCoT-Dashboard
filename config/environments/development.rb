f001742dd6c8d5b4b37d2e1c9b0e798462301306
MohamedAsan <mohamed.asan@freshworks.com>
2018-01-06 01:16:04 +0530

    Disable assets debugging

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -33,5 +33,7 @@ WcotDashboard::Application.configure do
   config.assets.compress = false
 
   # Expands the lines which load the assets
-  config.assets.debug = true
+  config.assets.debug = false
+
+  config.assets.digest = true
 end
