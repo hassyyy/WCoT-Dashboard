426ee71ed063d98f4556131cf428b4851da19a82
MohamedAsan <mohamed.asan@freshworks.com>
2018-01-10 22:36:20 +0530

    Comment out log

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -1,8 +1,9 @@
+// Handle Show/Hide {Alternative: Bootstrap Collapse[https://www.w3schools.com/bootstrap/bootstrap_collapse.asp]}
 $(function() {
   $('[show-hide-trigger]').click(function(event){
     event.preventDefault();
     elements_selector = "div[show-hide-elements='"+this.getAttribute('show-hide-trigger')+"']"
-    console.log(elements_selector)
+    // console.log(elements_selector)
     $(elements_selector).toggle();
   });
 });
