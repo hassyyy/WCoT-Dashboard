2bf46e14d96cf94d298234c5fcf9830f250b439f
MohamedAsan <mohamed.asan@freshworks.com>
2018-03-18 00:18:21 +0530

    Add Views and Assets for Meetings

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -2,5 +2,7 @@
 //= require jquery_ujs
 //= require bootstrap-datepicker
 //= require bootstrap-wysihtml5
+//= require jquery.timepicker.js
+//= require bootstrap-modal
 //= require bootstrap
 //= require_tree .
